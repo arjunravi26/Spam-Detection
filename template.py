d5c463b47800d7eaa60b0578e8739426f91c4a31
Arjun Ravi <arjunravi1523@gmail.com>
2024-05-22 21:36:42 +0530

    initial commit

---
+++ template.py
@@ -0,0 +1,45 @@
+import logging.config
+import os
+from pathlib import Path
+import logging                                                      
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+project_name = "text_summarizer"
+
+list_of_files = [
+    ".github/workflow/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/component/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/congig/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constant/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    filedir,file_name = os.path.split(file_path)
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {file_name}")
+        
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {file_path}")
+    else:
+        logging.info(f"Creating empty file: {file_path}")
+        

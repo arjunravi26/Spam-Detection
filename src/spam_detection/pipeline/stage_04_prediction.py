16a0e2417ea99aabd004a9561f31459b6421ef2a
Arjun Ravi <arjunravi1523@gmail.com>
2024-05-24 11:25:48 +0530

    Prediction Pipeline

--- src/spam_detection/pipeline/stage_04_prediction.py
+++ src/spam_detection/pipeline/stage_04_prediction.py
@@ -1,20 +1,30 @@
 from src.spam_detection.pipeline.stage_02_data_transformation import DataTransformationPipeline
 import pickle
 from src.spam_detection.config.configuration import ConfiguaraionManager
+
+
 class Prediction:
     def __init__(self) -> None:
-        pass
-    def preprocess(self,data):
-        preprocess_pipeline = DataTransformationPipeline()
-        preproecessed_data = preprocess_pipeline.predict(data)
-        return preproecessed_data
-    def model_predict(self,predict_data):
         config = ConfiguaraionManager()
         model_path_config = config.model_trainer()
-        model = pickle.load(model_path_config.model_path)
-        output = model.predict(predict_data)
+        self.model = pickle.load(model_path_config.model_path)
+        self.preprocess_pipeline = DataTransformationPipeline()
+
+    def preprocess(self, data):
+        """Preprocesses the data using the DataTransformationPipeline."""
+        if data is None:
+            raise ValueError("Data cannot be None.")
+        preprocessed_data = self.preprocess_pipeline.predict(data)
+        return preprocessed_data
+
+    def model_predict(self, predict_data):
+        """Makes a prediction using the preloaded model."""
+        if predict_data is None:
+            raise ValueError("Predict data cannot be None.")
+        output = self.model.predict(predict_data)
         return output
-    def start(self,data):
+
+    def start(self, data):
         predict_data = self.preprocess(data)
         output = self.model_predict(predict_data)
         return output

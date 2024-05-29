1f2c5fedefd04c6f0643ee4afbdbe232ee98454a
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2024-05-29 16:28:37 +0530

    modified main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import warnings
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     spam_dectector = SpamDetector()
-    spam_dectector.train()
+    ## Uncomment if you want to retrain the model
+    # spam_dectector.train()
     msg = input("Enter a message to check if it is spam or not: ")
     if msg:
         print(spam_dectector.detect(message=msg))
